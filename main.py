ee219c5829b143889b9ba3de528436658ab03ab4
thulanimbatha <thulanimbatha06@gmail.com>
2022-04-12 10:26:57 +0200

    opened csv file using Pandas

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import csv
+import pandas
 
 with open("weather_data.csv") as data_file:
     # creates csv reader object
     data = csv.reader(data_file)
+    temperatures = []
     # loop through each row
     for row in data:
         print(row)
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+    print(temperatures)
+print("\nCSV files using Pandas:\n")
+data1 = pandas.read_csv("weather_data.csv")
+print(data1)
+print("These are the temperatures:\n")
+print(data1["temp"])
